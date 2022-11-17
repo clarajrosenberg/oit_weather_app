ab950fbeb5169b63ced15ed339d719239dd31ff2
clarajrosenberg <33033258+clarajrosenberg@users.noreply.github.com>
2022-11-16 21:04:44 -0500

    weather app

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # oit_weather_app
+
+A program that takes input of a location and outputs the day's weather forecast. It does this by first making an API call to determine the latitude & longitude of the place that most closely matches the input given, and then making a second API call to get the daily weather forecast for those coordinates.
+
+In order to run this successfully, you will need to create two API keys and store them in a file named keys.py:
+- [OpenWeather](https://openweathermap.org/api)
+- [PositionStack](https://positionstack.com/product)
+
+This is an expansion of CodeWithTomi/FreeCodeCamp's tutorial [Getting Weather Information](https://www.youtube.com/watch?v=SqvVm3QiQVk&t=1494s).
+
+I coded this as one of multiple mini-projects during [Out in Tech](https://outintech.com/)'s fall 2022 mentorship program. I chose this project to gain practice making API calls as part of a larger pipeline.
---
+++ app.py
@@ -0,0 +1,45 @@
+import requests
+from pprint import pprint
+from keys import location_API_key, weather_API_key
+
+
+def get_lat_long(location):
+    location_base_url = "http://api.positionstack.com/v1/forward?access_key="\
+                        + location_API_key + "&query=" + location
+    location_data = requests.get(location_base_url).json()
+    lat = str(location_data["data"][0]["latitude"])
+    long = str(location_data["data"][0]["longitude"])
+
+    return lat, long
+
+
+def kelv_to_fahr(temp_kelv):
+    temp_fahr = round(1.8*(temp_kelv-273) + 32)
+
+    return temp_fahr
+
+
+def get_weather(lat, long):
+    weather_base_url = "http://api.openweathermap.org/data/2.5/weather?appid="\
+                        + weather_API_key + "&lat=" + lat + "&lon=" + long
+    weather_data = requests.get(weather_base_url).json()
+
+    forecast = weather_data["weather"][0]["description"]
+    feels_like = kelv_to_fahr(weather_data["main"]["feels_like"])
+    temp_max = kelv_to_fahr(weather_data["main"]["temp_max"])
+    temp_min = kelv_to_fahr(weather_data["main"]["temp_min"])
+    output_list = ["Today's forecast is ", forecast, ". It currently feels like ",
+                   str(feels_like), " with a high of ", str(temp_max),
+                   " and a low of ", str(temp_min), "."]
+    print("".join(output_list))
+
+
+def main():
+    location = input("Enter a location: ")
+    lat, long = get_lat_long(location)
+    get_weather(lat, long)
+    print("Seem wrong? Try being more specific (e.g. 'Springfield MA' instead of 'Springfield').")
+
+
+if __name__ == "__main__":
+    main()
